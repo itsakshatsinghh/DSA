d89c9a7bc51cea8ddac839a3f964e04fda4d487d
Akshat Singh <ranaakshatprof45@gmail.com>
2025-11-03 20:56:49 +0530

    Time: 4 ms (21.67%), Space: 95.7 MB (31.72%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+        vector<int> merged;
+        merged.reserve(nums1.size()+nums2.size());
+        //merging
+        int i=0,j=0;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i]<nums2[j]){
+                merged.push_back(nums1[i++]);
+            }else{
+                merged.push_back(nums2[j++]);
+            }
+        }
+        //discarding the extraa
+        while(i<nums1.size()){
+           merged.push_back(nums1[i++]);
+        }
+        while(j<nums2.size()){
+           merged.push_back(nums2[j++]);
+        }
+        //median calc
+        int n=merged.size();
+        if(n%2==1){
+            return merged[n/2];
+        }else{
+            int m1=merged[(n/2)-1];
+            int m2=merged[n/2];
+            return (m1+m2)/2.0;
+        }
+
+    }
+};
