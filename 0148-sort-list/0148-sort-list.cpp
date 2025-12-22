748f1d4439bfbeff9ea82a92838c013e1697839e
Akshat Singh <ranaakshatprof45@gmail.com>
2025-12-22 12:25:32 +0530

    Time: 4 ms (97.74%), Space: 57.1 MB (74.09%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* getMid(ListNode* head){
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+    //merging function 
+    ListNode* mergesort(ListNode* left , ListNode* right){
+        ListNode dummy(0);
+        ListNode* tail=&dummy;
+
+        while(left && right){
+            if(left->val<right->val){
+                tail->next=left;
+                left=left->next;
+            }else{
+                tail->next=right;
+                right=right->next;
+            }
+            tail=tail->next;
+        }
+        tail->next = (left) ? left : right;
+
+        return dummy.next;
+    }
+
+
+    ListNode* sortList(ListNode* head) {
+        if(!head || !head->next){
+            return head;
+        }
+        ListNode* mid= getMid(head);
+        ListNode* left= head;
+        ListNode* right= mid->next;
+        //breaking that link 
+        mid->next=NULL;
+
+        //recursion to break till one
+        left=sortList(left);
+        right=sortList(right);
+        //reuturing after merging unitl final
+        return mergesort(left , right);
+        
+    }
+};
