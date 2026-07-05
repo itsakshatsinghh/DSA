c8bb3fa59095828568819b359258cea3c4af7814
Akshat Singh <ranaakshatprof45@gmail.com>
2026-07-05 09:14:16 +0530

    Time: 9 ms (48.34%), Space: 11.4 MB (23.95%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -10,25 +10,28 @@ class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
         //first check the cycle ( the point where f and s collides and after than once its confirmed then move the f to head but keep the s pointer where the collision happens) now move the s pointer one step and also the f pointer one step , they will collide again where the the thing has happend - keep the index and return it 
-        if(!head || !head->next){
-            return nullptr;
+        
+        //////////// x2
+        if(!head|| !head->next ){
+            return NULL;
         }
         ListNode* s = head;
         ListNode* f = head;
-        bool cycle = false;
-        while(f!=nullptr && f->next!=nullptr){
-            s= s->next;
-            f= f->next->next;
+        bool hascycl = false;
+        while(f!=NULL && f->next!=NULL){
+            s=s->next;
+            f=f->next->next;
+            
             if(s==f){
-                //cycle detected 
-                cycle = true;
+                hascycl = true;
                 break;
             }
         }
-        if(!cycle){
+        if(!hascycl){
             return nullptr;
         }
-        f= head;
+        //we need to find the index so reset f = head 
+        f=head;
         while(s!=f){
             s=s->next;
             f=f->next;
