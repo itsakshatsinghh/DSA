1bb54a3aece23f232aa5acf4384668e65d438d44
Akshat Singh <ranaakshatprof45@gmail.com>
2026-07-13 11:33:04 +0530

    Time: 0 ms (100%), Space: 8.2 MB (76.91%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -20,3 +20,29 @@ public:
 };
 
 //if we want to use sliding window we need to make a string and then using that we can solve the question 
+/*
+class Solution {
+public:
+    int len(int n){
+        int count=0;
+        while(n){
+            n/=10;
+            count++;
+        }
+        return count;
+    }
+    string s="123456789";
+    vector<int> sequentialDigits(int low, int high) {
+        int lowLen=len(low);
+        int highLen=len(high);
+        vector<int> ans;
+        for(int len=lowLen;len<=highLen;len++){
+            for(int i=0;i<9-len+1;i++){
+                int num=stoi(s.substr(i,len));
+                if(num>=low and num<=high) ans.push_back(num);
+            }
+        }
+        return ans;
+    }
+};
+*/
