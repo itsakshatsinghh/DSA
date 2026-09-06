a2abd4d2eb6fb3d4a13a325f2e904148590d8c8d
Akshat Singh <ranaakshatprof45@gmail.com>
2026-09-07 02:23:53 +0530

    Time: 0 ms (100%), Space: 7.9 MB (80.12%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        return helper(n,k)+1;
+    }
+    int helper(int n, int k){
+        int ans = 0;
+        for(int i = 1 ; i<=n ; i++){
+            ans = (ans + k)%i;
+
+        }
+        return ans;
+    }
+};
